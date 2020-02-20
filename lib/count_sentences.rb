c1b316b150b9b678d46916b9340cd782cd9e0c06
industrious-generator-6081 <graywolf.niehaus@live.com>
2020-02-20 20:59:18 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -1,8 +1,6 @@
-require 'pry'
-
 class String
 
-def sentence?
+  def sentence?
     self.end_with?(".")
   end
 
@@ -13,8 +11,16 @@ def sentence?
   def exclamation?
     self.end_with?("!")
   end
-
+  
   def count_sentences
-    Self.split(/[.!?]/).map{|x| }
+    self.split(/[.!?]/).reject {|x| x.empty?}.size
   end
+  
+  def count_sentences_words
+    self.split(/[.!?]/).map{|x| !(x.match(/\w+/).nil?)}.reject{|x| x == false}.size 
+    # splits string into Array, collects those would-be sentences that actually contain word characters, removes false values, then counts the remaining booleans.
+  end
+
 end
+
+"Well, I don't know, is this a sentence?? ??? What about this?".count_sentences_words
